de49fcb6c6928350accb2bf96a061ae97a60a3a1
Ratisha <ratishasmith@gmail.com>
2023-09-25 18:23:14 -0400

    created test branch for team

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { TestbranchComponent } from './testbranch/testbranch.component';
 
-const routes: Routes = [{path:'login', component:LoginComponent}];
+const routes: Routes = [
+  {path:'login', component:LoginComponent}, 
+  {path:'testbranch', component:TestbranchComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
