10066523e0f6c891e34b4d09f9052032356bd3cf
Ratisha <ratishasmith@gmail.com>
2023-10-15 11:47:34 -0400

    code examples

--- src/app/models/testfullname.ts
+++ src/app/models/testfullname.ts
@@ -1,6 +1,7 @@
 
 //Interfaces are custom data types for your app. "string" is a data type however we created a custom object data type called TestFullName
-export interface TestFullName{
-    firstname:string | null;
-    lastname:string | null;
+export interface FamilyName{
+    id?:number;
+    firstname?:string | null;
+    lastname?:string | null;
 }
