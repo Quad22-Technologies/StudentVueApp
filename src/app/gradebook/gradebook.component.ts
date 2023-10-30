f70d2c57d15e699ecbcf372d9e040390b0ed754b
DerrickJzantt <derrickzantt2005@gmail.com>
2023-10-30 18:18:38 -0400

    the navbar and grade book components update
    the navbar is linked to the registration and also the logins html components.

--- src/app/gradebook/gradebook.component.ts
+++ src/app/gradebook/gradebook.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Gradebook } from '../models/gradebook';
+import { GradeBookService } from '../services/gradebook.service';
 
 @Component({
   selector: 'app-gradebook',
@@ -6,5 +8,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./gradebook.component.scss']
 })
 export class GradebookComponent {
-
+gradebooklist:Gradebook[] =[];
+constructor(private gradebookservice: GradeBookService){
+  this.gradebooklist = gradebookservice.getGradeBookData();
+}
 }
