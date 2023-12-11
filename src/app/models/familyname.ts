8a21c5c1a65c1f47d4c8fb718ddac2f2aaee0449
Ratisha <ratishasmith@gmail.com>
2023-12-11 16:17:52 -0500

    code clean up

--- src/app/models/familyname.ts
+++ src/app/models/familyname.ts
@@ -1,7 +1,7 @@
 
 //Interfaces are custom data types for your app. "string" is a data type however we created a custom object data type called TestFullName
 export interface FamilyName{
-    id?:number;
+    id?: string;
     firstname?:string | null;
     lastname?:string | null;
 }
--- src/app/testbranch/testbranch.component.ts
+++ src/app/testbranch/testbranch.component.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
-import { FamilyName} from '../models/testfullname'; // imported interface so we can use it
-import { MyFamilyService } from '../services/my-family.service';
-import { BackendConnectFamilyNameService } from '../services/backend-connect-family-name.service';
+import { FamilyName} from '../models/familyname'; // imported interface so we can use it
+import { FamilyNameDataService } from '../services/familynamedataservice';
 
 
 @Component({
@@ -16,54 +15,76 @@ export class TestbranchComponent {
   // FormGrop Tracks the value and validity state of a group of FormControl instances
   //When instantiating a FormGroup, pass in a collection of child controls as the first argument. The key for each child registers the name for the control.
   form = new FormGroup({
-    controlnamelist: new FormControl(''),
     firstName: new FormControl(''),
     lastName: new FormControl(''),
-    controlbackendnamelist: new FormControl(''), //Form control for backend data list dropdown
-    controldatabasebackendnamelist: new FormControl('')
+    familynamelist: new FormControl('')
   });
   
    //instantiate an empty object (family name). 
-  fullname: FamilyName = {};
-  familyNameList: FamilyName[] = [];
-  backendfamilyNameList: FamilyName[] = []; //Array of objects to hold data from the backend service
+  familyname: FamilyName = {};
   databasebackendfamilyNameList: FamilyName[] = [];
 
 //inside this contructor we are inializing the MyFamilyService class object so it can be used thoughout this component
 //If you navigate to the MyFamilyService Class you will see the @Injectable- this decorator that marks a class as available to be provided and injected as a dependency.
 //We are INJECTING MyFAMILYSERVICE Dependency into this component. 
-  constructor(private familynameservice: MyFamilyService, private backendFamilyService: BackendConnectFamilyNameService){}
+  constructor(private familyNameDataService: FamilyNameDataService){}
 
   //ngOnInit is a life cycle hook called by Angular to indicate that the Angular is DONE creating the component
   ngOnInit(): void {
-   
-    //Pulls from frontend only and write data to the familyNameList array
-    this.familyNameList = this.familynameservice.getFamilyNamesData();
-    
-    //Pulls from backend only and write data to the backendfamilyNameList array
-    this.backendFamilyService.getFamilyNames().subscribe(data => {
-                                              console.log("Backend Data List", data);
-                                              this.backendfamilyNameList = data; });
-
-    //Pulls from backend and database and write data to the databasebackendfamilyNameList array
-    this.backendFamilyService.getDatabaseFamilyNames().subscribe(data => {
-                                                this.databasebackendfamilyNameList = data; }); 
+    this.loadFamilyNames();
+ 
   }
 
-  //function that captures form values on the click event. Look into the html.
-  showFormValues() // function call from the button click
-  {
-    this.fullname.firstname = this.form.controls.firstName.value;
-    this.fullname.lastname = this.form.controls.lastName.value;
-
-     console.log("Data from the object - ", this.fullname)
-     console.log("Datafrom the controls - ", this.form.controls.firstName.value, this.form.controls.lastName.value);
-
-   }
+   loadFamilyNames() {
+    this.familyNameDataService.getDatabaseFamilyNames().subscribe(
+      data => this.databasebackendfamilyNameList = data,
+      error => console.error('Error loading family names:', error)
+    );
+  }
    
+  createFamilyName() {
+
+    // Check if the form is valid before proceeding ** Added Validation
+    if (this.form.valid) {
+      // Assuming this.form is an instance of FormGroup
+      this.familyname.firstname = this.form.controls.firstName.value;
+      this.familyname.lastname = this.form.controls.lastName.value;
+  
+      this.familyNameDataService.createFamilyName(this.familyname).subscribe(
+        createdFamilyName => {
+          console.log('Family name created successfully:', createdFamilyName);
+          this.loadFamilyNames(); // Reload the list after creating a new family name
+          this.form.reset(); // Reset the form controls
+        },
+        error => {
+          console.error('Error creating family name:', error);
+          // Provide user feedback, e.g., display an error message
+        }
+      );
+    } else {
+      // Form is not valid, provide user feedback or handle accordingly
+    }
+  }
+  
    //function that is called on the select dropdown change event in the html
-   changeFamilyNameList(e:any) {
-     console.log(e.target.value);
-     console.log(e.target.value);
+   changeFamilyNameList(e: any) {
+    console.log(e.target.value);
   }
+  
+  //function that is called on the select dropdown change event in the html
+  selectedFamilyName(e: any) {
+    console.log(e);
+    
+  }
+
+  deleteFamilyName(e: any) {
+    console.log(e);
+    this.familyNameDataService.deleteFamilyName(e.id).subscribe(
+      () => {
+        this.loadFamilyNames(); // Reload the list after deleting a family name
+        this.form.reset(); // Reset the form controls
+      }
+    );
+  }
+
 }
