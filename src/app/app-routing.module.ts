503b4c14ffd1c647ae1826281e0bd94041ec5c28
MKeita2005 <kingsmohamed152@gmail.com>
2024-01-15 18:49:10 -0500

    test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
-import { TestbranchComponent } from './testbranch/testbranch.component';
+import { TestbranchComponent } from './familynames/testbranch.component';
 import { GradebookComponent } from './gradebook/gradebook.component';
+import { ScheduleComponent } from './schedule/schedule.component';
 
 const routes: Routes = [
   {path:'', component:LoginComponent},
   {path:'testbranch', component:TestbranchComponent},
   {path:'registration', component:RegistrationComponent},
-  {path:'Gradebook', component:GradebookComponent}
+  {path:'Gradebook', component:GradebookComponent},
+  {path:'schedule', component:ScheduleComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { TestbranchComponent } from './testbranch/testbranch.component';
+import { TestbranchComponent } from './familynames/testbranch.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { LoginComponent } from './login/login.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { GradebookComponent } from './gradebook/gradebook.component';
 import { HttpClientModule } from '@angular/common/http';
+import { ScheduleComponent } from './schedule/schedule.component';
 
 
 @NgModule({
@@ -17,7 +18,8 @@ import { HttpClientModule } from '@angular/common/http';
     LoginComponent,
     TestbranchComponent,
     RegistrationComponent,
-    GradebookComponent
+    GradebookComponent,
+    ScheduleComponent
 
   ],
   imports: [
